16f6a4c9016909fd122428a2043f91625c0718c3
Beatriz Lúcia Teixeira de Souza <125310668+bealucia@users.noreply.github.com>
2023-09-18 09:12:00 -0300

    module

--- date_dif.py
+++ date_dif.py
@@ -1,6 +1,11 @@
-import datetime 
 
-def diferenca_date(data_1, data_2):
+def diferenca_date_console(data_1, data_2):
+    
+    diferenca_days = abs((data_2 - data_1).days)
+    
+    return diferenca_days
+
+def diferenca_date_arquivo(datas):
     
     diferenca_days = abs((data_2 - data_1).days)
     
