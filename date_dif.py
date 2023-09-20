becaa559e8df20d29f4934cf9e788793902066b0
Beatriz Lúcia Teixeira de Souza <125310668+bealucia@users.noreply.github.com>
2023-09-20 08:12:17 -0300

    Add files via upload

--- date_dif.py
+++ date_dif.py
@@ -1,23 +1,26 @@
-import datetime
+
+from datetime import datetime
 
 def diferenca_date_console():
-    
     entrada = input("Digite as datas no formato 'dia de mês de ano': ")
-    #data1, data2 = entrada.split(" - ")
-   # diferenca_days = abs((data2 - data1).days)
-    #return data1.strip(), data2.strip()
-    #return diferenca_days
-    #convertendo as datas
+    
     data1_str, data2_str = entrada.split(" - ")
 
     data1 = datetime.strptime(data1_str, "%d de %B de %Y")
-    data2  = datetime.strptime(data2_str, "%d de %B de %Y")
+    data2 = datetime.strptime(data2_str, "%d de %B de %Y")
     
-    return data2 - data1
+    diferenca = abs((data2 - data1).days)
+    
+    return diferenca
 
 def diferenca_date_arquivo(datas_arquivo):
     with open(datas_arquivo, "r", encoding="utf-8") as file:
         conteudo = file.read()
         data1_str, data2_str = conteudo.split(" - ")
-    return data1_str.strip(), data2_str.strip()
+        
+        data1 = datetime.strptime(data1_str, "%d de %B de %Y")
+        data2 = datetime.strptime(data2_str, "%d de %B de %Y")
+        
+        diferenca = abs((data2 - data1).days)
     
+    return diferenca
