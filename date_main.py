bab778061f8d68cae540ac139340f5d082874a9e
Beatriz Lúcia Teixeira de Souza <125310668+bealucia@users.noreply.github.com>
2023-09-20 08:00:58 -0300

    Add files via upload

--- date_main.py
+++ date_main.py
@@ -1,21 +1,14 @@
 import date_dif as dt
-from datetime import datetime
+
+tipo_de_envio = input("como você quer enviar as datas? [console/arquivo]")
+
+if tipo_de_envio == "console":
+    dt.diferenca_date_console()
+    print(dt.diferenca_date_console())
+
+elif tipo_de_envio == "arquivo":
+    dt.diferenca_date_arquivo()
+    print(dt.diferenca_date_arquivo())
 
 
-# recebendo as datas pelo console 
-date1 = input("digite uma data: ")
-print("a data digitada foi" , date1 , " digite outra data ")
-date2 = input()
-print("as datas digitadas foram: " , date1, "e",date2)  
 
-#convertendo as datas
-data_1  = datetime.strptime(date1, "%d de %B de %Y")
-data_2  = datetime.strptime(date2, "%d de %B de %Y")
-
-dt.diferenca_date_console(data_1, data_2)
-print(" a número de dias entre as datas é: ", dt.diferenca_date_console(data_1, data_2))
-
-# recebendo por arquivo de texto 
-datas = open("datas.txt")
-datas = print(datas.read())
-dt.diferenca_date_arquivo(datas)
